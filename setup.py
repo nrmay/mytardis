84222c15def821cd313d17a2f60c919fe18b66f0
Nicholas May <nicholas.may@rmit.edu.au>
2014-04-24 12:09:23 +1000

    Fix incorrect egg version.

--- setup.py
+++ setup.py
@@ -49,11 +49,11 @@ setup(
         'ua-parser==0.3.3',
         'PyYAML==3.10',
         'django-tastypie==0.9.16-tzfix',
-        'django-cas-provider==0.3.1-depfix',
+        'django-cas-provider==0.3.2-depfix',
         #'pygraphviz',  # for automatic diagram generation for the docs
     ],
     dependency_links=[
         'https://github.com/grischa/django-tastypie/tarball/testing-mytardis-deployment#egg=django-tastypie-0.9.16-tzfix',  # noqa
-        'https://github.com/nrmay/django-cas-provider/tarball/depfix#egg=django-cas-provider-0.3.1-depfix',
+        'https://github.com/nrmay/django-cas-provider/tarball/depfix#egg=django-cas-provider-0.3.2-depfix',
     ]
 )
