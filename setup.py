1de8112e3790341bb83468d97090eeffc19f9f3a
Nicholas May <nicholas.may@rmit.edu.au>
2014-07-23 16:12:31 +1000

    update setuptools to version 5.0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     packages=find_packages(),
     namespace_packages=['tardis'],
     install_requires=[
-        'setuptools==3.4.4',
+        'setuptools==5.0',
         'lxml==3.2.1',
         'pyparsing==1.5.7',  # held back by rdflib and rdfextras
         'feedparser==5.1.3',
